5258ca9f9b096018a7448a3d3e8624e432e37058
hfchou <tim_chou@chowsangsang.com>
2019-12-17 16:30:36 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,77 +1,35 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Sidebar from "./Sidebar";
-import "./style.css";
+import Cart from "./Cart";
 
+const cartItems = [
+  {
+    imgUrl:
+      "https://cdn.chowsangsang.com/dfs/UAT/ivCssModelImages/90754/44aacc26b80d09543d087d16425d6dc7.jpg",
+    collection: "Fingers Play 「指玩」",
+    name: "18K黃金戒指",
+    options: [
+      { name: "數量", value: "1" },
+      { name: "材質", value: "18K" },
+      { name: "紅色黃金，長度", value: "45厘米" }
+    ]
+  },
+  {
+    imgUrl:
+      "https://cdn.chowsangsang.com/dfs/UAT/ivCssModelImages/91302/759c105974897ccb59dccd6aa5ded818.jpg",
+    collection: "Fingers Play 「指玩」",
+    name: "18K黃金戒指",
+    options: [
+      { name: "數量", value: "1" },
+      { name: "材質", value: "18K" },
+      { name: "紅色黃金，長度", value: "45厘米" }
+    ]
+  }
+];
 class App extends React.Component {
-  state = {
-    moveIn: false,
-    showLightbox: false
-  };
-  click = () => {
-    this.setState({
-      moveIn: true
-    });
-  };
-  clickShowLightbox = () => {
-    this.setState({
-      showLightbox: true
-    });
-  };
-  moveOut = () => {
-    this.setState({
-      moveIn: false
-    });
-  };
-  closeLightbox = () => {
-    this.setState({
-      showLightbox: false
-    });
-  };
-  get title() {
-    return (
-      <>
-        <span className="icon-block icon icon-cart"></span>我的購物車
-      </>
-    );
-  }
-  get emptyCart() {
-    return (
-      <div className="empty-cart">
-        <div className="group-empty">
-          <p className="row-icon">
-            <span className="icon icon-cart"></span>
-          </p>
-          <p className="row1">一切還好嗎?</p>
-          <p className="row2">你的購物袋是空的。</p>
-        </div>
-        <div className="row-button">
-          <button>了解最新商品</button>
-        </div>
-      </div>
-    );
-  }
   render() {
     return (
       <div className="App">
-        <div style={{ borderBottom: "1px solid grey" }}>
-          <button onClick={this.click}>open right</button>
-        </div>
-        <Sidebar
-          show={this.state.moveIn}
-          close={this.moveOut}
-          title={this.title}
-        >
-          {this.emptyCart}
-        </Sidebar>
-        <div
-          style={{ border: "1px solid red", width: "480px", padding: "40px" }}
-        >
-          AAAAAA
-        </div>
-
-        <button>KKKK</button>
+        <Cart cartItems={cartItems} />
       </div>
     );
   }
---
+++ src/Cart.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import Sidebar from "hf-sidebar";
+import "./css/cart.css";
+import "./css/style.css";
+import pdtImg from "../src/img/pdt.jpg";
+
+export default class Cart extends Component {
+  state = {
+    moveIn: false
+  };
+  openSidebar = () => {
+    this.setState({
+      moveIn: true
+    });
+  };
+  closeSidebar = () => {
+    this.setState({
+      moveIn: false
+    });
+  };
+  get title() {
+    return (
+      <>
+        <span className="icon-block icon icon-cart"></span>我的購物車
+      </>
+    );
+  }
+  get emptyCart() {
+    return (
+      <div className="empty-cart">
+        <div className="group-empty">
+          <p className="row-icon">
+            <span className="icon icon-cart"></span>
+          </p>
+          <p className="row1">一切還好嗎?</p>
+          <p className="row2">你的購物袋是空的。</p>
+        </div>
+        <div className="row-button">
+          <button>了解最新商品</button>
+        </div>
+      </div>
+    );
+  }
+  get cartContent() {
+    const { cartItems } = this.props;
+
+    return (
+      <div className="cart-content">
+        <div className="cart-subtotal">
+          <span className="cart-subtotal-name">小計</span>
+          <span className="cart-subtotal-price">$1,400.00</span>
+        </div>
+        <button className="checkout-btn">{`結帳${cartItems.length}件商品`}</button>
+        <div className="cart-content-body">
+          <table className="cart-table">
+            <tbody>
+              {cartItems.map((item, index) => (
+                <tr key={index}>
+                  <td className="cartItem-img">
+                    {/*商品圖片 */}
+                    <img src={item.imgUrl}></img>
+                  </td>
+                  <td className="cartItem-text">
+                    {/*商品title*/}
+                    <p className="cartItem-text-title">{item.collection}</p>
+                    <p className="cartItem-text-title">{item.name}</p>
+                    {/*商品選項，數量*/}
+                    <p className="cartItem-text-detail">
+                      {item.options.map((text, index) => (
+                        <span key={index}>
+                          {text.name}：{text.value}
+                        </span>
+                      ))}
+                    </p>
+                    {/*商品價格*/}
+                    <p className="cartItem-text-price">HK$1,400.00</p>
+                  </td>
+                  {/*移除按鈕*/}
+                  <td className="cartItem-remove">
+                    <button className="cartItem-remove-btn"></button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    const { cartItems } = this.props;
+
+    return (
+      <div className="cart">
+        <div style={{ borderBottom: "1px solid grey" }}>
+          <button onClick={this.openSidebar}>open right</button>
+        </div>
+        <Sidebar
+          show={this.state.moveIn}
+          close={this.closeSidebar}
+          title={this.title}
+        >
+          {cartItems.length > 0 ? this.cartContent : this.emptyCart}
+        </Sidebar>
+      </div>
+    );
+  }
+}
+
+Cart.defaultProps = {
+  cartItems: []
+};
